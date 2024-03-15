64f5ecdffaba6301cfbb3823705bae926ddab23d
Varick Lim <varicklsr@gmail.com>
2024-03-15 17:11:58 +0800

    add table

--- app/ui/TokenList.tsx
+++ app/ui/TokenList.tsx
@@ -1,10 +1,10 @@
 import { createClient } from "@/helpers/supabase/server";
 import { cookies } from "next/headers";
-import { Box } from "@radix-ui/themes";
+import { Box, Table, Text } from "@radix-ui/themes";
+import dayjs from "dayjs";
 
 export default async function TokenList(props) {
   const { date } = props;
-  console.log("date", date.toString());
 
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
@@ -16,9 +16,30 @@ export default async function TokenList(props) {
 
   return (
     <Box>
-      {tokens?.map(({ address }) => (
-        <Box key={address}>{address}</Box>
-      ))}
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Token</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Alert time</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell width="40%">Price</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Price change</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {tokens?.map(({ address, created_at }) => (
+            <Table.Row key={address}>
+              <Table.RowHeaderCell>
+                <Box>Name $NAME</Box>
+                <Text size="1">{address}</Text>
+              </Table.RowHeaderCell>
+              <Table.Cell>{dayjs(created_at).format("hh:mm A")}</Table.Cell>
+              <Table.Cell>chart</Table.Cell>
+              <Table.Cell>%50</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </Box>
   );
 }
