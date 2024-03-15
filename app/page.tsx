63c28ec28377f3bbb9dd02796412c1a96d49297e
Varick Lim <varicklsr@gmail.com>
2024-03-15 15:21:28 +0800

    add radix ui lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Box, Heading, Text } from "@radix-ui/themes";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <Box>
+      <Heading>Alerts</Heading>
+      <Box>
+        <Text>Alerts will go here</Text>
+      </Box>
+    </Box>
+  );
 }
