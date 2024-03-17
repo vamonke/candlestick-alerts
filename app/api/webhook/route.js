2f0701fc32b7147265b348a919e44f5a86cd5c7d
Varick Lim <varicklsr@gmail.com>
2024-03-17 13:47:55 +0800

    webhook

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -2,7 +2,7 @@ import { webhookCallback } from "grammy";
 import bot from "../../../bot";
 
 export const POST = async (request) => {
-  console.log("request");
+  console.log("request", request);
   webhookCallback(bot, "std/http")(request);
   return Response.json({ ok: true });
 };
