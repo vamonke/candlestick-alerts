207e299363229b07bcdfc9d7544835a39fa1c95a
Varick Lim <varicklsr@gmail.com>
2024-03-17 14:23:54 +0800

    wip

--- app/api/webhook/telegram/route.js
+++ app/api/webhook/telegram/route.js
@@ -5,8 +5,8 @@ import bot from "../../../../bot";
 export const POST = async (request) => {
   noStore();
   console.log("📩 Telegram webhook");
-  webhookCallback(bot, "std/http")(request);
-  return Response.json({ ok: true });
+  return webhookCallback(bot, "std/http")(request);
+  // return Response.json({ ok: true });
 };
 
 // curl -F "url=https://candlestick-alerts.vercel.app/api/webhook/telegram" https://api.telegram.org/bot<BOT_TOKEN>/setWebhook
